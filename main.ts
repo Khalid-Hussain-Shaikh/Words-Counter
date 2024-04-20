d9c8066886af08fdda3412a79049162384d369a4
Khalid-Hussain-Shaikh <hussainibneali00@gmail.com>
2024-04-20 16:40:04 +0500

    adding a CLI Words Counter.

---
+++ main.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node 
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log("=".repeat(60))
+console.log(chalk.bold.cyanBright("\tWelcome to Khalid Shaikh - Words Counter"))
+console.log("=".repeat(60))
+const answers :{
+    Sentence:string
+} = await inquirer.prompt([
+    {
+        type: "input",
+        name: "Sentence",
+        message: "Enter your Sentence to count the words:"
+    }
+])
+const words = answers.Sentence.trim().split(" ")
+console.log("=".repeat(60))
+console.log(words)
+console.log(chalk.yellow(`Your sentence word count is: ${words.length}`));
+console.log("=".repeat(60))
+
+
+
